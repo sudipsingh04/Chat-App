9d77bb53b44ef02cefc80178bfdf47b2b15cd0bc
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-08 17:26:37 +0530

    Initial commit

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCbeMZaK-xsGCGC7fNm5AYEjlX-YGkcHmY",
+  authDomain: "sudip-chat.firebaseapp.com",
+  databaseURL: "https://sudip-chat.firebaseio.com",
+  projectId: "sudip-chat",
+  storageBucket: "sudip-chat.appspot.com",
+  messagingSenderId: "682473177592",
+  appId: "1:682473177592:web:e46fdbc114cd3b9eb9160d",
+  measurementId: "G-3Q7T3DRXXH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
