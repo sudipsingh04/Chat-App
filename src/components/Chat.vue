01bea3bcde8ed9e88f08c05b9f5693854844649f
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-08 19:35:53 +0530

    Modified chat vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="chat container">
-    <h2>Chat {{ this.name
-       }}</h2>
+    <h2 class="center teal-text">Vue Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +30,16 @@ export default {
 }
 </script>
 <style>
-
+.chat h2{
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span{
+  font-size: 1.4em;
+}
+.chat .time{
+  display: block;
+  font-size: 1.2em;
+}
 </style>
 
